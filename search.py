b4b729bfc9b2c1b13353eb6186932986589c83b3
Eric L Sammons <esammons@redhat.com>
2021-09-03 12:47:00 -0700

    pylint fixes and additional formatting

--- search.py
+++ search.py
@@ -1,3 +1,8 @@
-search = "@RedHat OR #RedHat OR #LifeatRedHat OR @RedHatJobs"
-days = 7 # 7 days is the maximum
+"""Contains variables to be used in twitteretl.ipynb.
+"""
 
+# What do you want to search for
+SEARCH = "@RedHat OR #RedHat OR #LifeatRedHat OR @RedHatJobs"
+
+# How many days of content do you want, 7  is the max
+DAYS = 7
